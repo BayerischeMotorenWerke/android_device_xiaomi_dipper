7b08080e38e82725ea198adfd4630a388f1579c1
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2026-05-31 21:30:19 +0700

    dipper: Include GameBar overlay
    * Add GameBar performance monitoring overlay
    * Enable GameBar in product configuration
    
    Change-Id: I63574269d00b85fb793966c1b34cfb30de68a370

--- device.mk
+++ device.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/dipper/dipper-vendor.mk)
 
+# GameBar
+$(call inherit-product, packages/apps/GameBar/gamebar.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2248
 TARGET_SCREEN_WIDTH := 1080
